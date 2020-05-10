ded0198c401ae8c88a49e39710537fdec537837e
Carlos Mertens <mertenscarlos@yahoo.com>
2020-05-10 23:17:04 +0200

    Refactor function component Card into a class component

--- Card.js
+++ Card.js
@@ -1,17 +1,17 @@
 // Create Card component
-function Card(props) {
-  const title = props.data.course;
+/* function Card(props) {
   function salesOn() {
     return false;
   }
   return (
     <div className="col s3">
-      <div className="card hoverable medium">
+      <div className="card hoverable large">
         <div className="card-image">
           <img src={props.data.image} />
         </div>
         <div className="card-content">
-          <p>"{title}"</p>
+          <p>"{props.data.course}"</p>
+          <br />
           <p>{props.data.instructor}</p>
         </div>
         <div className="card-action">
@@ -20,4 +20,30 @@ function Card(props) {
       </div>
     </div>
   );
+} */
+
+class Card extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="col s3">
+        <div className="card hoverable large">
+          <div className="card-image">
+            <img src={this.props.data.image} />
+          </div>
+          <div className="card-content">
+            <p>"{this.props.data.course}"</p>
+            <br />
+            <p>{this.props.data.instructor}</p>
+          </div>
+          <div className="card-action">
+            <a href="#">€9.99</a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
